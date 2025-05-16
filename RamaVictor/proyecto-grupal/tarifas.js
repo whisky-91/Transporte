2cbb2f93a9071c8d61221e4f1b871c18bb7eba46
Brian-Artur <brianarturfaro@gmail.com>
2025-05-16 23:51:52 +0200

    Separar tarifas

---
+++ RamaVictor/proyecto-grupal/tarifas.js
@@ -0,0 +1,8 @@
+export const tarifas = {
+    KmTerrestre: 2,
+    KmMaritimo: 1,
+    KgTerrestre: 0.5,
+    KgMarino: 0.2,
+    MaxPeso: 5000
+};
+//# sourceMappingURL=tarifas.js.map
---
+++ RamaVictor/proyecto-grupal/tarifas.ts
@@ -0,0 +1,11 @@
+/**
+ * Las tarifas van en un objeto literal para quen en un futurose puedan con 
+ * facilidad. Es posible que nos interese agregar más metios de transporte. 
+ ***/
+export const tarifas = {
+  KmTerrestre: 2,
+  KmMaritimo: 1,
+  KgTerrestre: 0.5,
+  KgMarino: 0.2,
+  MaxPeso: 5000
+};

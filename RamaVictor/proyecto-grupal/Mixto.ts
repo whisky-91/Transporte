72024400d73449b119faa6e994d73c22f922795c
Brian-Artur <brianarturfaro@gmail.com>
2025-05-16 23:54:19 +0200

    Separar clase Mixto (espacial)

---
+++ RamaVictor/proyecto-grupal/Mixto.ts
@@ -0,0 +1,27 @@
+import { Transporte } from "./Transporte.js";
+import { Terrestre } from "./Terrestre.js";
+import { Maritimo } from "./Maritimo.js";
+
+export class Mixto {
+  //propiedades propias de Mixto para almacenar los kilometros terrestres y marítimos
+  rutaTerrestre: Transporte;
+  rutaMaritima: Transporte;
+  peso: number;
+
+  constructor(kmTerrestres: number, kmMaritimos: number, peso: number) {
+    //sumamos los kmTerrestres y maritimos para que en el constructor padre se pase la distancia total, que es lo que recibe.
+
+    this.rutaTerrestre = new Terrestre();
+    this.rutaMaritima = new Maritimo();
+    this.peso = peso;
+  }
+
+  calcularPrecio(): number {
+    let costoMaritimo = this.rutaMaritima.calcularPrecio()
+    let costoTerrestre = this.rutaTerrestre.calcularPrecio()
+    //Calculamos el precio
+    let precio = costoTerrestre + costoMaritimo;
+    precio = Number(precio.toFixed(2))  // 2 decimales
+    return precio;
+  }
+}
